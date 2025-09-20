8f8d35f0f1a509a63ea83b161a3268d6df229a5c
Rafael Freitas <rafael@tag.mx>
2025-09-20 02:12:25 -0300

    Refatoração de código: padronização de aspas e melhorias na legibilidade do código no arquivo directive.ts

--- src/directive.ts
+++ src/directive.ts
@@ -1,4 +1,4 @@
-import { DirectiveBinding, ExtractPropTypes } from 'vue';
+import { DirectiveBinding, ExtractPropTypes } from "vue";
 import {
   debug,
   filterOptRestrictions,
@@ -7,10 +7,10 @@ import {
   setCursor,
   validateRestrictedOptions,
   event,
-} from './Utils';
-import format from './format';
-import unformat from './unformat';
-import defaults, { VMoneyOptions } from './options';
+} from "./Utils";
+import format from "./format";
+import unformat from "./unformat";
+import defaults, { VMoneyOptions } from "./options";
 
 // this option is used for ALL directive instances
 // let opt: VMoneyOptions = defaults;
@@ -18,12 +18,16 @@ import defaults, { VMoneyOptions } from './options';
 const setValue = (
   el: HTMLInputElement,
   opt: VMoneyOptions | ExtractPropTypes<never>,
-  caller: string,
+  caller: string
 ) => {
-  debug(opt, 'directive setValue() - caller', caller);
+  debug(opt, "directive setValue() - caller", caller);
 
   if (!validateRestrictedOptions(opt)) {
-    debug(opt, 'directive setValue() - validateRestrictedOptions() return false. Stopping here...', el.value);
+    debug(
+      opt,
+      "directive setValue() - validateRestrictedOptions() return false. Stopping here...",
+      el.value
+    );
     return;
   }
 
@@ -41,34 +45,38 @@ const setValue = (
 
   setCursor(el, positionFromEnd);
 
-  el.dispatchEvent(event('change')); // v-model.lazy or not
+  el.dispatchEvent(event("change")); // v-model.lazy or not
 };
 
-const onKeyDown = (e: KeyboardEvent, opt: VMoneyOptions | ExtractPropTypes<never>) => {
+const onKeyDown = (
+  e: KeyboardEvent,
+  opt: VMoneyOptions | ExtractPropTypes<never>
+) => {
   const el = e.currentTarget as HTMLInputElement;
 
-  const backspacePressed = e.code === 'Backspace' || e.code === 'Delete';
-  const isAtEndPosition = (el.value.length - (el.selectionEnd || 0)) === 0;
+  const backspacePressed = e.code === "Backspace" || e.code === "Delete";
+  const isAtEndPosition = el.value.length - (el.selectionEnd || 0) === 0;
 
-  debug(opt, 'directive onkeydown() - el.value', el.value);
-  debug(opt, 'directive onkeydown() - backspacePressed', backspacePressed);
-  debug(opt, 'directive onkeydown() - isAtEndPosition', isAtEndPosition);
+  debug(opt, "directive onkeydown() - el.value", el.value);
+  debug(opt, "directive onkeydown() - backspacePressed", backspacePressed);
+  debug(opt, "directive onkeydown() - isAtEndPosition", isAtEndPosition);
 
-  if (opt.allowBlank
-      && backspacePressed
-      && isAtEndPosition
-      && unformat(el.value, opt, 'directive onkeydown allowBlank') === 0
+  if (
+    opt.allowBlank &&
+    backspacePressed &&
+    isAtEndPosition &&
+    unformat(el.value, opt, "directive onkeydown allowBlank") === 0
   ) {
     debug(opt, 'directive onkeydown() - set el.value = ""', el.value);
-    el.value = '';
-    el.dispatchEvent(event('change')); // v-model.lazy
+    el.value = "";
+    el.dispatchEvent(event("change")); // v-model.lazy
   }
 
-  debug(opt, 'directive onkeydown() - e.key', e.key);
-  if (e.key === '+') {
-    debug(opt, 'directive onkeydown() - unformat el.value', el.value);
-    let number = unformat(el.value, opt, 'directive onkeydown +');
-    if (typeof number === 'string') {
+  debug(opt, "directive onkeydown() - e.key", e.key);
+  if (e.key === "+") {
+    debug(opt, "directive onkeydown() - unformat el.value", el.value);
+    let number = unformat(el.value, opt, "directive onkeydown +");
+    if (typeof number === "string") {
       number = parseFloat(number);
     }
     if (number < 0) {
@@ -79,17 +87,17 @@ const onKeyDown = (e: KeyboardEvent, opt: VMoneyOptions | ExtractPropTypes<never
 
 const onInput = (e: Event, opt: VMoneyOptions | ExtractPropTypes<never>) => {
   const el = e.currentTarget as HTMLInputElement;
-  debug(opt, 'directive oninput()', el.value);
+  debug(opt, "directive oninput()", el.value);
   if (/^[1-9]$/.test(el.value)) {
     el.value = numbersToCurrency(el.value, fixed(opt.precision));
-    debug(opt, 'directive oninput() - is 1-9', el.value);
+    debug(opt, "directive oninput() - is 1-9", el.value);
   }
-  setValue(el, opt, 'directive oninput');
+  setValue(el, opt, "directive oninput");
 };
 
 const onFocus = (e: Event, opt: VMoneyOptions | ExtractPropTypes<never>) => {
   const el = e.currentTarget as HTMLInputElement;
-  debug(opt, 'directive onFocus()', el.value);
+  debug(opt, "directive onFocus()", el.value);
   if (opt.focusOnRight) {
     setCursor(el, el.value.length - opt.suffix.length);
   }
@@ -97,10 +105,12 @@ const onFocus = (e: Event, opt: VMoneyOptions | ExtractPropTypes<never>) => {
 
 const getValidatedInputElement = (el: HTMLInputElement): HTMLInputElement => {
   // v-money3 used on a component that's not an input
-  if (el.tagName.toLocaleUpperCase() !== 'INPUT') {
-    const els = el.getElementsByTagName('input');
+  if (el.tagName.toLocaleUpperCase() !== "INPUT") {
+    const els = el.getElementsByTagName("input");
     if (els.length !== 1) {
-      throw new Error(`v-money3 requires 1 input, found ${els.length} elements.`);
+      throw new Error(
+        `v-money3 requires 1 input, found ${els.length} elements.`
+      );
     } else {
       // eslint-disable-next-line prefer-destructuring
       return els[0];
@@ -109,7 +119,10 @@ const getValidatedInputElement = (el: HTMLInputElement): HTMLInputElement => {
   return el;
 };
 
-const registerListeners = (el: HTMLInputElement, opt: VMoneyOptions | ExtractPropTypes<never>) => {
+const registerListeners = (
+  el: HTMLInputElement,
+  opt: VMoneyOptions | ExtractPropTypes<never>
+) => {
   el.onkeydown = (e: KeyboardEvent) => {
     onKeyDown(e, opt);
   };
@@ -134,28 +147,31 @@ export default {
       ...binding.value,
     });
 
-    debug(opt, 'directive mounted() - opt', opt);
+    debug(opt, "directive mounted() - opt", opt);
 
     el = getValidatedInputElement(el);
 
     registerListeners(el, opt);
 
-    debug(opt, 'directive mounted() - el.value', el.value);
-    setValue(el, opt, 'directive mounted');
+    debug(opt, "directive mounted() - el.value", el.value);
+    setValue(el, opt, "directive mounted");
   },
   updated(el: HTMLInputElement, binding: DirectiveBinding): void {
     if (!binding.value) {
       return;
     }
 
+    // grantee that el is an input element - for Element-UI el-input usage
+    el = getValidatedInputElement(el);
+
     const opt: VMoneyOptions | ExtractPropTypes<never> = filterOptRestrictions({
       ...defaults,
       ...binding.value,
     });
 
-    debug(opt, 'directive updated() - opt', opt);
-    debug(opt, 'directive updated() - el.value', el.value);
-    setValue(el, opt, 'directive updated');
+    debug(opt, "directive updated() - opt", opt);
+    debug(opt, "directive updated() - el.value", el.value);
+    setValue(el, opt, "directive updated");
   },
   beforeUnmount(el: HTMLInputElement): void {
     el.onkeydown = null;
